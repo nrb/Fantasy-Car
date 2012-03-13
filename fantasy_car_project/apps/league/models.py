54fdb6156090892fc67bfff8410a85512535b50b
Nolan Brubaker <nolan@nbrubaker.com>
2012-03-12 22:35:14 -0400

    Add first pass of models.

--- fantasy_car_project/apps/league/models.py
+++ fantasy_car_project/apps/league/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Season(models.Model):
+    year = models.IntegerField()
+
+
+class Track(models.Model):
+    name = models.CharField(max_length=80)
+    city = models.CharField(max_length=80)
+    length = models.IntegerField()
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=80)
+    length = models.IntegerField()
+    date = models.DateField()
+    
+    track = models.ForeignKey(Track, related_name="races")
+    season = models.ForeignKey(Season, related_name="races")
+
+
+class Driver(models.Model):
+    name = models.CharField(max_length=100)
+    number= models.IntegerField()
+    
+    seasons = models.ManyToManyField(Season, related_name="drivers")
+
